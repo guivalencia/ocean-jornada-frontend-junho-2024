119de9ad0922e5699e4fccb062c9cdd71cf160c6
Guilherme Valencia <57770213+guivalencia@users.noreply.github.com>
2024-06-13 12:14:00 +0000

    refatorando

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
- return (
-  <>
-    <div className="card">
-      <h2>Rick Sanchez</h2>
-      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
-    </div>
-  </>
- )
+  const item1 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+  const item2 = {
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
+  const item3 = {
+    name: 'Summer Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+
+  // Listas (Arrays)
+  const itens = [item1, item2, item3]
+
+  return (
+    <>
+      <div className="cards">
+        {/* Para cada um dos itens da lista, exibir um Card */}
+        {itens.map(elemento => <Card item={elemento} key={elemento.name} />)}
+      </div>
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,17 @@
+export default function Card(props) {
+  const item = props.item
+
+  return (
+    <div className="card">
+      <h2>{item.name}</h2>
+
+      <div className="tags">
+        <div>Status: Vivo</div>
+        <div>Espécie: Humana</div>
+        <div>Origem: Terra C-137</div>
+      </div>
+
+      <img src={item.image} />
+    </div>
+  )
+}
