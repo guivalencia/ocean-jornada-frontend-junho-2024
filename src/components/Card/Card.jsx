b79eeed3cb17d128df150334f46a30c2e6d075a5
Guilherme Valencia <57770213+guivalencia@users.noreply.github.com>
2024-06-14 14:15:55 +0000

    Link adicionado nos Cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom"
+
 export default function Card(props) {
   const item = props.item
 
   return (
-    <div className="card">
-      <h2>{item.name}</h2>
+    <Link to={`/item/${item.id}`}>
+      <div className="card">
+        <h2>{item.name}</h2>
 
-      <div className="tags">
-        <div>Status: Vivo</div>
-        <div>Espécie: Humana</div>
-        <div>Origem: Terra C-137</div>
+        <div className="tags">
+          <div>Status: Vivo</div>
+          <div>Espécie: Humana</div>
+          <div>Origem: Terra C-137</div>
+        </div>
+
+        <img src={item.image} />
       </div>
-
-      <img src={item.image} />
-    </div>
+    </Link>
   )
 }
