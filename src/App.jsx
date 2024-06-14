2f23989aafef23f43baff7099c20b43ef70929e8
Guilherme Valencia <57770213+guivalencia@users.noreply.github.com>
2024-06-14 12:47:26 +0000

    Criação das rotas iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>
+  },
+  {
+    path: '/teste',
+    element: <div>Teste</div>
+  }
+])
 
 function App() {
   return (
     <>
-     <Home></Home>
+     <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
