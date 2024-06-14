d4fba89278b354c820d553c23d88c1cbe9b25a58
Guilherme Valencia <57770213+guivalencia@users.noreply.github.com>
2024-06-14 15:00:52 +0000

    Incrementando ViewById

--- src/pages/ViewItemById.jsx
+++ src/pages/ViewItemById.jsx
@@ -3,8 +3,7 @@ import { Link, useParams } from 'react-router-dom'
 import './ViewItemById.css'
 
 function ViewItemById() {
-
-  const [item, setItens] = useState([])
+  const [item, setItem] = useState()
 
   const { itemId } = useParams()
 
@@ -15,28 +14,57 @@ function ViewItemById() {
 
     const body = await response.json()
 
-    setItens(body)
+    setItem(body)
   }
 
-  useEffect(function() {
+  useEffect(function () {
     carregarDados()
   }, [])
 
-  if(!item){
+  if (!item) {
     return <div>Carregando...</div>
   }
 
   return (
     <>
-    <div className="view_item">
-      <Link to="/">Voltar para Home</Link>
-      <h1>{item.name}</h1>
+      <div className="view_item">
+        <Link to="/">Voltar para Home</Link>
 
-      <div className="content">
-        <div>Imagem</div>
-        <div>Texto</div>
+        <h1>{item.name}</h1>
+
+        <div className="content">
+          <div>
+            <img src={item.image} />
+          </div>
+
+          <div className="attributes">
+            <div className="attribute">
+              <h2>STATUS</h2>
+              <p>{item.status}</p>
+            </div>
+
+            <div className="attribute">
+              <h2>SPECIES</h2>
+              <p>{item.species}</p>
+            </div>
+
+            <div className="attribute">
+              <h2>GENDER</h2>
+              <p>{item.gender}</p>
+            </div>
+
+            <div className="attribute">
+              <h2>ORIGIN</h2>
+              <p>{item.origin.name}</p>
+            </div>
+
+            <div className="attribute">
+              <h2>LOCATION</h2>
+              <p>{item.location.name}</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
     </>
   )
 }
